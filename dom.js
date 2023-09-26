2c0b627a65b93cc71b016c0cf54336225df61230
lohith-glpl <127127206+lohith-glpl@users.noreply.github.com>
2023-09-26 14:30:48 +0530

    Update-add-remove dom.js

--- dom.js
+++ dom.js
@@ -89,7 +89,7 @@ window.onload=()=>{
 //        even[i].style.backgroundColor = '#ccc';     
 // }
 
-var itemList = document.querySelector('#items');
+// var itemList = document.querySelector('#items');
 //parentNode 
 // console.log(itemList.parentNode)
 //  itemList.parentNode.style.backgroundColor = '#f4f4f4';
@@ -144,5 +144,56 @@ var itemList = document.querySelector('#items');
 // var h1 = document.querySelector('header h1');
 // console.log(newDiv);
 
-container.insertBefore(newDiv, h1)
+// container.insertBefore(newDiv, h1)
+
+var form = document.getElementById('addForm');
+    var itemList = document.getElementById('items');
+   
+
+    // Form submit event
+    form.addEventListener('submit', addItem);
+    // Delete event
+    itemList.addEventListener('click', removeItem);
+   
+
+
+    // Add item
+    function addItem(e){
+    e.preventDefault();
+
+    // Get input value
+    var newItem = document.getElementById('item').value;
+
+    // Create new li element
+    var li = document.createElement('li');
+    // Add class
+    li.className = 'list-group-item';
+    // Add text node with input value
+    li.appendChild(document.createTextNode(newItem));
+
+    // Create del button element
+    var deleteBtn = document.createElement('button');
+
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to list
+    itemList.appendChild(li);
+    }
+
+    // Remove item
+    function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        if(confirm('Are You Sure?')){
+        var li = e.target.parentElement;
+        itemList.removeChild(li);
+        }
+    }
+    }
 }
